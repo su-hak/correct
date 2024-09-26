f26e24603dd7cf36506c27f7d2fb84d770289523
kimsuhak <uiouij@naver.com>
2024-09-27 02:15:13 +0900

    last

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { UsersModule } from './users/users.module';
 import * as path from 'path';
 import { GrammarModule } from './grammar/grammar.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthGuard } from './auth/auth.guard';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -27,8 +25,8 @@ import { APP_GUARD } from '@nestjs/core';
             password: matches[2],
             database: matches[5],
             entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false, // Heroku 환경에서는 synchronize를 false로 설정
-            logging: true, // 쿼리 로깅 활성화
+            synchronize: false,
+            logging: true,
           };
         } else {
           return {
@@ -40,7 +38,7 @@ import { APP_GUARD } from '@nestjs/core';
             database: configService.get('DB_NAME'),
             entities: [__dirname + '/**/*.entity{.ts,.js}'],
             synchronize: configService.get('DB_SYNC') === 'true',
-            logging: true, // 쿼리 로깅 활성화
+            logging: true,
           };
         }
       },
@@ -50,11 +48,5 @@ import { APP_GUARD } from '@nestjs/core';
     GrammarModule,
     AuthModule
   ],
-  providers: [
-    {
-    provide: APP_GUARD,
-    useClass: AuthGuard,
-  }
-],
 })
 export class AppModule {}
