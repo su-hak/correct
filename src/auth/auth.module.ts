02d7419019cab1afa487a336a81abdbdcbe83d9f
kimsuhak <uiouij@naver.com>
2024-09-27 01:26:59 +0900

    2024-09-27 01:26

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,5 @@
 // src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from '../auth/auth.service';
 import { AuthController } from '../auth/auth.controller';
 import { UsersModule } from '../users/users.module';
