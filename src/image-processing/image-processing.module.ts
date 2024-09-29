45992211d5f0eface9aedbb6adc43b7eed702a8e
kimsuhak <uiouij@naver.com>
2024-09-29 22:52:41 +0900

    image-processing module에도 HttpModule추가

--- src/image-processing/image-processing.module.ts
+++ src/image-processing/image-processing.module.ts
@@ -5,13 +5,15 @@ import { BullModule } from '@nestjs/bull';
 import { GrammarService } from 'src/grammar/grammar.service';
 import { GrammarModule } from 'src/grammar/grammar.module';
 import { ImageProcessingProcessor } from './image-processing.processor';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'image-processing',
     }),
-    GrammarModule
+    GrammarModule,
+    HttpModule,
   ],
   controllers: [ImageProcessingController],
   providers: [VisionService, GrammarService, ImageProcessingProcessor],
