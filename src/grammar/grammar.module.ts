8d285757dee6ee9f534ef654f6c05a2d5d9268a4
kimsuhak <uiouij@naver.com>
2024-09-29 22:47:40 +0900

    grammar module에 HttpModule추가

--- src/grammar/grammar.module.ts
+++ src/grammar/grammar.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { GrammarController } from './grammar.controller';
 import { GrammarService } from './grammar.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    HttpModule,
+  ],
   controllers: [GrammarController],
   providers: [GrammarService],
   exports: [GrammarService]
