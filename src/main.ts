54cbff89cf1f56b1906aba6c89b18ebf77932c62
kimsuhak <uiouij@naver.com>
2024-10-03 23:37:42 +0900

    main ts 에 pipe추가

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,12 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
   const config = new DocumentBuilder()
     .setTitle('Community API')
     .setDescription('The community API description')
