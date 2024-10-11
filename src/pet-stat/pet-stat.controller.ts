9a0a2a75ccf1cb578cadb3a4ed732f316f1177bc
kimsuhak <uiouij@naver.com>
2024-10-12 01:37:06 +0900

    jwt를 pet-stat 컨트롤러에서 제거

--- src/pet-stat/pet-stat.controller.ts
+++ src/pet-stat/pet-stat.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Post, UseGuards, Body } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { PetStatService } from './pet-stat.service';
 
 @Controller('pet-stat')
-@UseGuards(AuthGuard('jwt'))
 export class PetStatController {
   constructor(private readonly petStatService: PetStatService) {}
 
