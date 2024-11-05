0a69322135e5a5f4ce4a2faecffccef4340cc409
kimsuhak <uiouij@naver.com>
2024-11-06 01:10:55 +0900

    t

--- src/grammar/grammar.module.ts
+++ src/grammar/grammar.module.ts
@@ -3,14 +3,20 @@ import { GrammarController } from './grammar.controller';
 import { GrammarService } from './grammar.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GrammarLearning } from './entities/grammar-Learning.entity';
+import { GrammarLearningService } from './grammar-Learning.service';
 
 @Module({
   imports: [
     AuthModule,
     HttpModule,
+    ConfigModule, // ConfigService를 사용하기 위해 필요
+    TypeOrmModule.forFeature([GrammarLearning])
   ],
   controllers: [GrammarController],
-  providers: [GrammarService],
+  providers: [GrammarService, GrammarLearningService],
   exports: [GrammarService]
 })
 export class GrammarModule {}
