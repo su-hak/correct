dbaec00e86db646d09e26f54f5cc25538f9347b4
kimsuhak <uiouij@naver.com>
2024-11-19 23:04:36 +0900

    맞춤법 학습 관련 로직 더 깔끔하게 제거

--- src/grammar/grammar.module.ts
+++ src/grammar/grammar.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { GrammarController } from './grammar.controller';
+/* import { GrammarController } from './grammar.controller'; */
 import { GrammarService } from './grammar.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 /* import { TypeOrmModule } from '@nestjs/typeorm';
 import { GrammarLearning } from './entities/grammar-Learning.entity';
-import { GrammarLearningService } from './grammar-Learning.service'; */
+import { GrammarLearningService } from './grammar-Learning.service'; 
 import { GrammarAdminController } from './grammarAdmin.controller';
-import { GrammarSeedService } from './grammarSeed.service';
+import { GrammarSeedService } from './grammarSeed.service'; */
 
 @Module({
   imports: [
@@ -17,16 +17,12 @@ import { GrammarSeedService } from './grammarSeed.service';
     ConfigModule, // ConfigService를 사용하기 위해 필요
   ],
   controllers: [
-    GrammarController,
-    GrammarAdminController
   ],
   providers: [
     GrammarService, 
-    GrammarSeedService
   ],
   exports: [
     GrammarService, 
-    GrammarSeedService,
   ]
 })
 export class GrammarModule {}
