76754dda74560f568b796e1ba3046b7093227c84
kimsuhak <uiouij@naver.com>
2024-11-20 19:09:53 +0900

    에러 수정 shared.service

--- src/shared/optimized-http.service.ts
+++ src/shared/optimized-http.service.ts
@@ -1,43 +1,44 @@
 // shared/optimized-http.service.ts
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import dns from 'dns';
-import https from 'https';
+import * as dns from 'dns';
+import * as https from 'https';  // 수정된 부분
 
 @Injectable()
 export class OptimizedHttpService {
-    private dnsCache = new Map();
-    private agent = new https.Agent({
-        keepAlive: true,
-        maxSockets: 100,
-        keepAliveMsecs: 10000
+  private dnsCache = new Map();
+  private agent: https.Agent;
+
+  constructor() {
+    this.agent = new https.Agent({
+      keepAlive: true,
+      maxSockets: 100,
+      keepAliveMsecs: 10000
+    });
+  }
+
+  createAxiosInstance(baseURL: string): AxiosInstance {
+    dns.setDefaultResultOrder('ipv4first');
+
+    const axiosInstance = axios.create({
+      baseURL,
+      timeout: 10000,
+      httpsAgent: this.agent,
+      headers: {
+        'Connection': 'keep-alive'
+      }
     });
 
-    // shared/optimized-http.service.ts
-    createAxiosInstance(baseURL: string): AxiosInstance {
-        dns.setDefaultResultOrder('ipv4first');
+    axiosInstance.interceptors.request.use(async (config) => {
+      const url = new URL(config.url);
+      if (!this.dnsCache.has(url.host)) {
+        const address = await dns.promises.resolve(url.host);
+        this.dnsCache.set(url.host, address[0]);
+      }
+      config.url = config.url.replace(url.host, this.dnsCache.get(url.host));
+      return config;
+    });
 
-        const axiosInstance = axios.create({
-            baseURL,
-            timeout: 10000,
-            httpAgent: this.agent,
-            httpsAgent: this.agent,
-            headers: {
-                'Connection': 'keep-alive'
-            }
-        });
-
-        // axios interceptor로 변경
-        axiosInstance.interceptors.request.use(async (config) => {
-            const url = new URL(config.url);
-            if (!this.dnsCache.has(url.host)) {
-                const address = await dns.promises.resolve(url.host);
-                this.dnsCache.set(url.host, address[0]);
-            }
-            config.url = config.url.replace(url.host, this.dnsCache.get(url.host));
-            return config;
-        });
-
-        return axiosInstance;
-    }
+    return axiosInstance;
+  }
 }
